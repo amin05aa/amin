e6480061bb3603380fa738c3d5352c6450c43f03
amin <eeonego3@gmail.com>
2020-04-01 19:31:12 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,13 @@
+﻿import telebot
+
+bot =  telebot.TeleBot("1057152705:AAGVuAgE5GpsiIkBLI78FxyavtJhVxyVQB4")
+
+@bot.message_handler( commands= ['start', ' help'])
+def send_welcome( message):
+ bot.reply_to(message, "Привет "chat_id"! Я Бот-Эхо!")
+
+@bot.message_handler( func= lambda message: True)
+def echo_all( message):
+ bot.reply_to(message, message.text)
+
+bot.polling()
